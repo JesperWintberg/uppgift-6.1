05a31a7ead1ae81ee351ddcbae68abe0b2a39e3d
JesperWintberg <jesper.wintberg@gmail.com>
2023-02-10 10:07:43 +0100

    Add project files.

---
+++ uppgift 6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addera(4, 6, 3);
+        }
+
+        static void addera(int tal1, int tal2, int tal3)
+        {
+            Console.WriteLine($"det kommer att bli {tal1} + {tal2} + {tal3} = {tal1 + tal2 +tal3}");
+        }
+    }
+}
+
